fcca762762043e2b4e314d4b8b4c5ffdcd1a332b
Guilherme775 <guilhermereis28122002@gmail.com>
2023-03-22 16:05:31 -0300

    lint: single quotes

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ['src/index.ts'],
   splitting: false,
   sourcemap: true,
   clean: true,
   dts: true,
-  format: ["cjs"],
+  format: ['cjs'],
 });
