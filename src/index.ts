b655d15113d596b1b54b5ca4926bdbe3ae7b44e7
Guilherme775 <guilhermereis28122002@gmail.com>
2023-03-27 18:07:14 -0300

    feat: add generator and imagine retrieval methods

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,22 @@ import FormData from 'form-data';
 
 import { prodApi, stagingApi } from '@/services/api';
 import { getSkyboxStylesResponse, generateSkyboxRequest, generateSkyboxResponse } from '@/schemas/skybox';
+import {
+  generateImagineRequest,
+  generateImagineResponse,
+  getGeneratorsResponse,
+  getImagineByIdRequest,
+  getImagineByIdResponse,
+  getImagineByObfuscatedIdRequest,
+  getImagineByObfuscatedIdResponse,
+  getImagineHistoryRequest,
+  getImagineHistoryResponse,
+} from '@/schemas/imagine';
+import { InternalError } from '@/utils/error';
 
 type BlockadeLabsSdkConstructor = {
   api_key: string;
   env?: 'production' | 'staging';
-};
-
-type Error = {
-  message: string;
 };
 
 export class BlockadeLabsSdk {
@@ -23,32 +31,34 @@ export class BlockadeLabsSdk {
     this.api = env === 'production' ? prodApi : stagingApi;
   }
 
-  async getSkyboxStyles(): Promise<z.infer<typeof getSkyboxStylesResponse> | Error> {
+  async getSkyboxStyles(): Promise<z.infer<typeof getSkyboxStylesResponse>> {
     try {
       const { data } = await this.api.get(`/skybox/styles?api_key=${this.api_key}`);
 
-      const validator = getSkyboxStylesResponse.safeParse(data);
+      const responseValidator = getSkyboxStylesResponse.safeParse(data);
 
-      if (validator.success === false) {
-        return {
-          message: validator.error.message,
-        };
+      if (responseValidator.success === false) {
+        throw new InternalError(responseValidator.error.message);
       }
 
-      return validator.data;
-    } catch (_err) {
-      return {
-        message: 'Unexpected error retrieving skybox styles',
-      };
+      return responseValidator.data;
+    } catch (err) {
+      if (err instanceof InternalError) throw new InternalError(err.message);
+
+      throw new InternalError('Unexpected error retrieving skybox styles');
     }
   }
 
-  async generateSkybox({
-    prompt,
-    skybox_style_id,
-    webhook_url,
-  }: z.infer<typeof generateSkyboxRequest>): Promise<z.infer<typeof generateSkyboxResponse> | Error> {
+  async generateSkybox(input: z.infer<typeof generateSkyboxRequest>): Promise<z.infer<typeof generateSkyboxResponse>> {
     try {
+      const requestValidator = generateSkyboxRequest.safeParse(input);
+
+      if (requestValidator.success === false) {
+        throw new InternalError(requestValidator.error.message);
+      }
+
+      const { prompt, skybox_style_id, webhook_url } = requestValidator.data;
+
       const formData = new FormData();
 
       formData.append('api_key', this.api_key);
@@ -63,19 +73,176 @@ export class BlockadeLabsSdk {
         headers: { 'Content-Type': 'multipart/form-data', 'Content-Length': `${formData.getLengthSync()}` },
       });
 
-      const validator = generateSkyboxResponse.safeParse(data);
+      const responseValidator = generateSkyboxResponse.safeParse(data);
 
-      if (validator.success === false) {
-        return {
-          message: validator.error.message,
-        };
+      if (responseValidator.success === false) {
+        throw new InternalError(responseValidator.error.message);
       }
 
-      return validator.data;
-    } catch (_err) {
-      return {
-        message: 'Unexpected error generating new skybox',
-      };
+      return responseValidator.data;
+    } catch (err) {
+      if (err instanceof InternalError) throw new InternalError(err.message);
+
+      throw new InternalError('Unexpected error generating new skybox');
+    }
+  }
+
+  async getGenerators(): Promise<z.infer<typeof getGeneratorsResponse>> {
+    try {
+      const { data } = await this.api.get(`/generators?api_key=${this.api_key}`);
+
+      const responseValidator = getGeneratorsResponse.safeParse(data);
+
+      if (responseValidator.success === false) {
+        throw new InternalError(responseValidator.error.message);
+      }
+
+      return responseValidator.data;
+    } catch (err) {
+      if (err instanceof InternalError) throw new InternalError(err.message);
+
+      throw new InternalError('Unexpected error retrieving generators');
+    }
+  }
+
+  async generateImagine(
+    input: z.infer<typeof generateImagineRequest>,
+  ): Promise<z.infer<typeof generateImagineResponse>> {
+    try {
+      const requestValidator = generateImagineRequest.safeParse(input);
+
+      if (requestValidator.success === false) {
+        throw new InternalError(requestValidator.error.message);
+      }
+
+      const { generator, generator_data, webhook_url } = requestValidator.data;
+
+      const formData = new FormData();
+
+      formData.append('api_key', this.api_key);
+      formData.append('generator', generator);
+
+      if (webhook_url) {
+        formData.append('webhook_url', webhook_url);
+      }
+
+      Object.entries(generator_data).map(([key, value]) => {
+        if (value instanceof Buffer) {
+          formData.append(key, value, { filename: key, contentType: 'application/octet-stream' });
+        } else {
+          formData.append(key, value);
+        }
+      });
+
+      const { data } = await this.api.post('/imagine/requests', formData, {
+        headers: { 'Content-Type': 'multipart/form-data', 'Content-Length': `${formData.getLengthSync()}` },
+      });
+
+      const responseValidator = generateImagineResponse.safeParse(data);
+
+      if (responseValidator.success === false) {
+        throw new InternalError(responseValidator.error.message);
+      }
+
+      return responseValidator.data;
+    } catch (err) {
+      if (err instanceof InternalError) throw new InternalError(err.message);
+
+      throw new InternalError('Unexpected error generating new imagine');
+    }
+  }
+
+  async getImagineById(input: z.infer<typeof getImagineByIdRequest>): Promise<z.infer<typeof getImagineByIdResponse>> {
+    try {
+      const requestValidator = getImagineByIdRequest.safeParse(input);
+
+      if (requestValidator.success === false) {
+        throw new InternalError(requestValidator.error.message);
+      }
+
+      const { id } = requestValidator.data;
+
+      const { data } = await this.api.get(`imagine/requests/${id}?api_key=${this.api_key}`);
+
+      const responseValidator = getImagineByIdResponse.safeParse(data);
+
+      if (responseValidator.success === false) {
+        throw new InternalError(responseValidator.error.message);
+      }
+
+      return responseValidator.data;
+    } catch (err) {
+      if (err instanceof InternalError) throw new InternalError(err.message);
+
+      throw new InternalError(`Unexpected error retrieving imagine: ${input.id}`);
+    }
+  }
+
+  async getImagineByObfuscatedId(
+    input: z.infer<typeof getImagineByObfuscatedIdRequest>,
+  ): Promise<z.infer<typeof getImagineByObfuscatedIdResponse>> {
+    try {
+      const requestValidator = getImagineByObfuscatedIdRequest.safeParse(input);
+
+      if (requestValidator.success === false) {
+        throw new InternalError(requestValidator.error.message);
+      }
+
+      const { obfuscated_id } = requestValidator.data;
+
+      const { data } = await this.api.get(`imagine/requests/obfuscated-id/${obfuscated_id}?api_key=${this.api_key}`);
+
+      const responseValidator = getImagineByObfuscatedIdResponse.safeParse(data);
+
+      if (responseValidator.success === false) {
+        throw new InternalError(responseValidator.error.message);
+      }
+
+      return responseValidator.data;
+    } catch (err) {
+      if (err instanceof InternalError) throw new InternalError(err.message);
+
+      throw new InternalError(`Unexpected error retrieving imagine: ${input.obfuscated_id}`);
+    }
+  }
+
+  async getImagineHistory(
+    input?: z.infer<typeof getImagineHistoryRequest>,
+  ): Promise<z.infer<typeof getImagineHistoryResponse>> {
+    try {
+      const requestValidator = getImagineHistoryRequest.safeParse(input);
+
+      if (requestValidator.success === false) {
+        throw new InternalError(requestValidator.error.message);
+      }
+
+      const url = (() => {
+        if (requestValidator.data) {
+          const searchParams = new URLSearchParams();
+
+          Object.entries(requestValidator.data).map(([key, value]) => {
+            searchParams.append(key, String(value));
+          });
+
+          return `imagine/myRequests?api_key=${this.api_key}&${searchParams.toString()}`;
+        }
+
+        return `imagine/myRequests?api_key=${this.api_key}`;
+      })();
+
+      const { data } = await this.api.get(url);
+
+      const responseValidator = getImagineHistoryResponse.safeParse(data);
+
+      if (responseValidator.success === false) {
+        throw new InternalError(responseValidator.error.message);
+      }
+
+      return responseValidator.data;
+    } catch (err) {
+      if (err instanceof InternalError) throw new InternalError(err.message);
+
+      throw new InternalError('Unexpected error retrieving imagine history');
     }
   }
 }
